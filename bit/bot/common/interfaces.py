506e6692737a320178c9ac83579445beb2f5b6b7
Ryan Northey <ryan@3ca.org.uk>
2011-12-22 23:20:21 +0000

    interface 8)

--- bit/bot/common/interfaces.py
+++ bit/bot/common/interfaces.py
@@ -1,8 +1,88 @@
 
 from zope.interface import Interface as I
 
+class ISQLQuery(I):
+    pass
 
-class IBitBotPluginFactory(I):
+class IFlatten(I):
+    pass
+
+class IDataBrain(I):
+    pass
+
+class IDataRecord(I):
+    pass
+
+class ISubscriptions(I):
+    pass
+
+class IIntelligent(I):
+    pass
+
+class IBotAgent(I):
+    pass
+
+class IAgents(I):
+    pass
+
+class IJabber(I):
+    pass
+
+class IHTMLResources(I):
+    pass
+
+class IWebImages(I):
+    pass
+
+class IWebFolder(I):
+    pass
+
+class IWebCSS(I):
+    pass
+
+class IWebJS(I):
+    pass
+
+class IWebHTML(I):
+    pass
+
+class IWebJPlates(I):
+    pass
+
+class IJPlates(I):
+    pass
+
+class ISockets(I):
+    pass
+
+class IJSON(I):
+    pass
+
+class IData(I):
+    pass
+
+class IDataProvider(I):
+    pass
+
+class ISessions(I):
+    pass
+
+class ISession(I):
+    pass
+
+class IHTTPRoot(I):
+    pass
+
+class IServices(I):
+    pass
+
+class IApplication(I):
+    pass
+
+class IPluginFactory(I):
+    pass
+
+class IPlugins(I):
     pass
 
 class IConfiguration(I):
